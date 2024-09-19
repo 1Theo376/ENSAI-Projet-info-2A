68f4d257fe8df5f322dca5eaea2ef2e14a28dc02
Emilien Morfouace <emilien.morfouace@eleve.ensai.fr>
2024-09-19 20:38:57 +0200

    tkt

---
+++ data/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO joueur(id_joueur, pseudo, mdp, age, mail, fan_pokemon) VALUES
+(999, 'admin','0000'),
+(998, 'a','a'),
+(997, 'maurice','1234'),
+(996, 'batricia','9876'),
+(995, 'miguel','abcd'),
+(994, 'gilbert','toto'),
+(993, 'junior','aaaa');
--- src/utilisateur.py
+++ src/utilisateur.py
@@ -15,18 +15,19 @@ class Utilisateur:
     mdp: str
         mot de passe rentré par l'utilisateur
 
+    pseudo: str
+        Pseudo rentré par l'utilisateur
+
     collec_phys: Collection_physique
         La collection physique de l'utilisateur
     """
-    def __init__(self, id: str, mdp: str, collec_phys: Collection_physique,
-                 collections: list[Collection]):
+    def __init__(self, id: str, mdp: str, pseudo: str):
         """
         Initialise une instance de Utilisateur
         """
         self.id = id
         self.mdp = mdp
-        self.collec_phys = collec_phys
-        self.collections = collections
+        self.pseudo = pseudo
 
     @staticmethod
     def creer_profil():
