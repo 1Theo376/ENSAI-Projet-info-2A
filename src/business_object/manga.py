2c75b462c88426c8a11cbb0411e47f81c8a573f7
fes7akma3alam <id2268@DOMENSAI.ECOLE>
2024-10-12 22:02:34 +0200

    manga

--- src/business_object/manga.py
+++ src/business_object/manga.py
@@ -1,3 +1,17 @@
 class Manga:
-    def __init__(self):
-        pass
+    def __init__(self,
+                 id_manga: int,
+                 titre: str,
+                 synopsis: str,
+                 auteurs: str,
+                 themes: list[str],
+                 genre: str):
+        self.id_manga = id_manga
+        self.titre = titre
+        self.synopsis = synopsis
+        self.auteurs = auteurs
+        self.themes = themes
+        self.genre = genre
+
+    def __str__(self) -> str:
+        return f"{self.titre} - Genre: {self.genre}, Auteurs: {self.auteurs}"
