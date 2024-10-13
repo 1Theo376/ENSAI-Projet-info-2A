5d77cc10054573d51106ae4fb789f0e7c350d8fd
Enguerrand Renon <enguerrand.renon@ensai.fr>
2024-10-13 15:22:14 +0200

    avis

--- src/business_object/avis.py
+++ src/business_object/avis.py
@@ -1,3 +1,24 @@
 class Avis:
-    def __init__(self):
-        pass
+    """
+    Définition de la classe avis
+
+    Attributs:
+    ----------------
+    id_avis : int
+        Identifiant lié à l'avis
+
+    texte : str
+        Contenu de l'avis
+    """
+    self.id_avis = id_avis
+    self.texte = texte
+
+    def __init__(self, id_avis:int, texte: str):
+        """
+        Retourne le contenu de l'avis
+
+        Retour : str
+        """  
+        return f"Avis ID: {self.id_avis}, Texte : {self.texte}"
+
+#test test
