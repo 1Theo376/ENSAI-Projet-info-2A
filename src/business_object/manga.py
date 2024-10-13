a3419b8bae844e79dd9b4e38892b38654224a461
Enguerrand Renon <enguerrand.renon@ensai.fr>
2024-10-13 15:35:58 +0200

    doc

--- src/business_object/manga.py
+++ src/business_object/manga.py
@@ -1,4 +1,8 @@
 class Manga:
+    """
+    Représente un manga avec ses informations de base
+    """
+
     def __init__(self,
                  id_manga: int,
                  titre: str,
@@ -6,6 +10,27 @@ class Manga:
                  auteurs: str,
                  themes: list[str],
                  genre: str):
+        """
+        
+        Initialise un manga
+
+    Parameters :
+    ----------
+    id_manga : int
+        L'identifiant unique du manga.
+    titre : str
+        Le titre du manga.
+    synopsis : str
+        Un résumé ou une description du manga.
+    auteurs : str
+        Les auteurs du manga.
+    themes : list[str]
+        Une liste de thèmes associés au manga (par exemple, aventure, drame).
+    genre : str
+        Le genre principal du manga (par exemple, action, romance).
+        
+        """
+
         self.id_manga = id_manga
         self.titre = titre
         self.synopsis = synopsis
@@ -14,4 +39,11 @@ class Manga:
         self.genre = genre
 
     def __str__(self) -> str:
+        """
+        Retourne une représentation en chaîne de caractères du manga.
+
+        Return : str
+        -------
+            Une chaîne de caractères formatée contenant le titre, le genre et les auteurs du manga.
+        """
         return f"{self.titre} - Genre: {self.genre}, Auteurs: {self.auteurs}"
