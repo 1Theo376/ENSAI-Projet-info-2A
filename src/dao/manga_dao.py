ffbab115f34052783ba02777a33fdfc3f016fd3c
fes7akma3alam <id2268@DOMENSAI.ECOLE>
2024-10-20 21:25:49 +0200

    mangadao

---
+++ src/dao/manga_dao.py
@@ -0,0 +1,47 @@
+import logging
+
+
+from dao.db_connection import DBConnection
+from manga import Manga
+
+
+class MangaDao:
+    """classe MangaDao"""
+
+    def trouver_manga_par_id(self, id_manga) -> Manga:
+        """Recherche et renvoie un manga par son id
+
+        Parameters
+        ---------
+        id_manga: int
+            numéro du manga qu'on veut rechercher
+
+        Returns
+        -------
+        manga: Manga
+            renvoie le manga
+        """
+        try:
+            with DBConnection().connection as connection:
+                with connection.cursor() as cursor:
+                    cursor.execute(
+                        "Select *",
+                        "From manga",
+                        "Where id_manga = %(id_manga)s ",
+                        {"id_manga": id_manga},
+                    )
+                    res = cursor.fetchone()
+        except Exception as e:
+            logging.info(e)
+            raise
+        manga = None
+        if res:
+            manga = Manga(
+                id_manga=res["id_manga"],
+                titre=res["titre"],
+                synopsis=res["synopsis"],
+                auteurs=res["auteurs"],
+                themes=res["themes"],
+                genre=res["genre"],
+            )
+        return manga
