89eedc55c0d0504abaf10c8b50bc5abc12d7dd82
Anais Babingui <ananashere0@gmail.com>
2024-10-21 16:59:54 +0200

    utilisateur

--- src/service/utilisateur_service.py
+++ src/service/utilisateur_service.py
@@ -1,78 +1,45 @@
-from tabulate import tabulate
-from security import hash_password
+# from tabulate import tabulate
+from utils.accueil.securite import hash_password
 from utilisateur import Utilisateur
-from dao.joueur_dao import JoueurDao
+from dao.utilisateur_dao import UtilisateurDao
 
 
-class JoueurService:
-    """Classe contenant les méthodes de service des Joueurs"""
-
-    def creer(self, pseudo, mdp, age, mail, fan_pokemon) -> Utilisateur:
-        """Création d'un joueur à partir de ses attributs"""
-
-        nouveau_joueur = Utilisateur(
-            pseudo=pseudo,
-            mdp=hash_password(mdp, pseudo),
-            id=id,
-        )
-
-        return nouveau_joueur if JoueurDao().creer(nouveau_joueur) else None
-
-    def lister_tous(self, inclure_mdp=False) -> list[Utilisateur]:
-        """Lister tous les joueurs
-        Si inclure_mdp=True, les mots de passe seront inclus
-        Par défaut, tous les mdp des joueurs sont à None
-        """
-        joueurs = JoueurDao().lister_tous()
-        if not inclure_mdp:
-            for j in joueurs:
-                j.mdp = None
-        return joueurs
-
-    def trouver_par_id(self, id_joueur) -> Utilisateur:
-        """Trouver un joueur à partir de son id"""
-        return JoueurDao().trouver_par_id(id_joueur)
-
-    def modifier(self, joueur) -> Utilisateur:
-        """Modification d'un joueur"""
-
-        joueur.mdp = hash_password(joueur.mdp, joueur.pseudo)
-        return joueur if JoueurDao().modifier(joueur) else None
-
-    def supprimer(self, joueur) -> bool:
-        """Supprimer le compte d'un joueur"""
-        return JoueurDao().supprimer(joueur)
-
-    def afficher_tous(self) -> str:
-        """Afficher tous les joueurs
-        Sortie : Une chaine de caractères mise sous forme de tableau
-        """
-        entetes = ["pseudo", "age", "mail", "est fan de Pokemon"]
-
-        joueurs = JoueurDao().lister_tous()
-
-        joueurs_as_list = [j.as_list() for j in joueurs]
-
-        str_joueurs = "-" * 100
-        str_joueurs += "\nListe des joueurs \n"
-        str_joueurs += "-" * 100
-        str_joueurs += "\n"
-        str_joueurs += tabulate(
-            tabular_data=joueurs_as_list,
-            headers=entetes,
-            tablefmt="psql",
-            floatfmt=".2f",
-        )
-        str_joueurs += "\n"
-
-        return str_joueurs
-
-    def se_connecter(self, pseudo, mdp) -> Utilisateur:
-        """Se connecter à partir de pseudo et mdp"""
-        return JoueurDao().se_connecter(pseudo, hash_password(mdp, pseudo))
+class UtilisateurService:
+    """Classe contenant les méthodes de service des Utilisateurs"""
 
     def pseudo_deja_utilise(self, pseudo) -> bool:
         """Vérifie si le pseudo est déjà utilisé
         Retourne True si le pseudo existe déjà en BDD"""
-        joueurs = JoueurDao().lister_tous()
-        return pseudo in [j.pseudo for j in joueurs]
+        utilisateurs = UtilisateurDao().lister_tous()
+        return pseudo in [j.pseudo for j in utilisateurs]
+
+    def creer_compte(self, pseudo, mdp) -> Utilisateur:
+        """Création d'un utilisateur à partir de ses attributs"""
+        nouveau_utilisateur = Utilisateur(
+            pseudo=pseudo,
+            mdp=hash_password(mdp, pseudo),
+            id=id,
+        )
+        if self.pseudo_deja_utilise(nouveau_utilisateur.pseudo):
+            raise ValueError("Ce pseudo est déjà utilisé, veuillez en choisir un autre")
+
+        return nouveau_utilisateur if UtilisateurDao().creer(nouveau_utilisateur) else None
+
+    def se_connecter(self, pseudo, mdp) -> Utilisateur:
+        """Se connecter à partir de pseudo et mdp"""
+        return UtilisateurDao().se_connecter(pseudo, hash_password(mdp, pseudo))
+
+    def supprimer_compte(self, utilisateur) -> bool:
+        """Supprimer le compte d'un utilisateur"""
+        if self.se_connecter(utilisateur.pseudo, utilisateur.mdp):
+            return UtilisateurDao().supprimer(utilisateur)
+        else:
+            return None
+
+    def modifier_compte(self, utilisateur) -> Utilisateur:
+        """Modification du compte d'un utilisateur"""
+        if self.se_connecter(utilisateur.pseudo, utilisateur.mdp):
+            utilisateur.mdp = hash_password(utilisateur.mdp, utilisateur.pseudo)
+            return utilisateur if Utilisateur().modifier(utilisateur) else None
+        else:
+            return None
