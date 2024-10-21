46b4a513e5029d2505382876e1609a4963d4da6a
fes7akma3alam <id2268@DOMENSAI.ECOLE>
2024-10-21 15:47:28 +0200

    rechercheservice

--- src/service/recherche_service.py
+++ src/service/recherche_service.py
@@ -8,12 +8,30 @@ class RechercheService:
     """Classe """
 
     def recherche_manga_par_titre(self, titre: str) -> Manga:
+        """Recherche les mangas par leurs titres
         
+        """
+        res = self.dao.mangadao.lister_tous[titre]
+        for man in res:
+            if man[titre] == titre:
+                return man
+            else:
+                print(f"Aucun manga trouvé pour le titre '{titre}'.")
 
     def recherche_utilisateur(self, pseudo):
-        """Recherche les utilisateurs par leurs pseudo
+        """Recherche les utilisateurs par leurs pseudos
+         Parameters
+        ----------
+        pseudo : str
+
+        Returns
+        -------
+        utilisateur : Utilisateur
+            renvoie la liste de tous les joueurs dans la base de données
         """
-        res = self.joueur_dao.lister_tous()
+
+        """
+        res = self.dao.joueur_dao.lister_tous()
         for pers in res:
             if pers[pseudo] == pseudo:
                 return pers
