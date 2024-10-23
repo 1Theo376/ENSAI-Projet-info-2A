de7c4c9e55db492cc1f380108926f4664728c69c
Enguerrand Renon <enguerrand.renon@ensai.fr>
2024-10-23 12:39:41 +0200

    vue profil

--- src/vues/profil_utilisateur_vue.py
+++ src/vues/profil_utilisateur_vue.py
@@ -1,15 +1,69 @@
 from InquirerPy import inquirer
-
 from vues.vue_abstraite import VueAbstraite
 from vues.session import Session
+from vues.collection_cohérente_vue import CollectionCohérenteVue
+from vues.collection_physique_vue import CollectionPhysiqueVue
+from vues.avis_utilisateur_vue import AvisUtilisateurVue
+from dao.collection_physique_dao import CollectionPhysiqueDAO
 
-class ProfilUtilisateurVue(VueAbstraite):
-     """Vue de l'écran du profil
+class EcranDuProfilVue(VueAbstraite):
+    """Vue de l'écran du profil
 
     Returns
     ------
     view
         retourne la prochaine vue, celle qui est choisie par l'utilisateur
     """
-    
+
     def choisir_menu(self):
+        """Choix du menu suivant de l'utilisateur
+
+        Return
+        ------
+        vue
+            Retourne la vue choisie par l'utilisateur dans le terminal
+        """
+
+        print("\n" + "-" * 50 + "\nÉcran du Profil\n" + "-" * 50 + "\n")
+
+        choix = inquirer.select(
+            message="Faites votre choix :",
+            choices=[
+                "Consulter mes collections cohérentes",
+                "Consulter ma collection physique",
+                "Consulter ses avis",
+                "Créer une collection cohérente",
+                "Créer une collection physique",
+                "Retour vers l'écran d'acceuil",
+                "Supprimer mon compte",
+            ],
+        ).execute()
+
+        match choix:
+            case "Consulter mes collections cohérentes":
+                return CollectionCohérenteVue()
+
+            case "Consulter ma collection physique":
+                return CollectionPhysiqueVue()
+
+            case "Consulter ses avis":
+                return AvisUtilisateurVue()
+
+            case "Créer une collection cohérente":
+                self.créer_collectionpys
+                message = inquirer.text(message="Collection physique créée : ").execute()
+                return CollectionPhysiqueVue()
+
+            case "Créer une collection physique":
+
+            case "Retour vers l'écran d'acceuil":
+                Session().deconnexion()
+                from vues.accueil.accueil_vue import AccueilVue
+                return AccueilVue()
+            case "Supprimer mon compte":
+
+
+
+
+
+
