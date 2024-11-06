f79b1a4edda47b58f82411a2606af066e9b6fbec
Anais Babingui <ananashere0@gmail.com>
2024-11-06 16:36:03 +0100

    ook

--- src/service/recherche_service.py
+++ src/service/recherche_service.py
@@ -24,7 +24,7 @@ class RechercheService:
             return sous_liste if sous_liste else f"Aucun manga trouvé pour l'indice {n}."
         return f"Aucun manga trouvé pour le titre '{titre}'."
 
-    def recherche_utilisateur(self, pseudo, n):
+    def recherche_utilisateur(self, pseudo, n, a):
         """Recherche un utilisateur par son pseudo
         Parameters
         ----------
@@ -38,6 +38,7 @@ class RechercheService:
         res = UtilisateurDao().rechercher_tous_pseudo(pseudo)
         if res:
             liste = [j.pseudo for j in res]
-            sous_liste = liste[n:n + 8]
-            return sous_liste if sous_liste else f"Aucun manga trouvé pour l'indice {n}."
+            sous_liste = liste[n:n + 8 + a]
+            long = len(sous_liste)
+            return {"longueur": long, "liste": sous_liste} if sous_liste else f"Aucun manga trouvé pour l'indice {n}."
         return f"Aucun manga trouvé pour l'indice {n}."
--- src/vues/rechercher_utilisateur_vue.py
+++ src/vues/rechercher_utilisateur_vue.py
@@ -23,8 +23,8 @@ class RechercheUtilisateurVue(VueAbstraite):
     def choisir_menu(self):
         """Choix du menu suivant de l'utilisateur
 
-        Return
-        ------
+        Returns
+        -------
         vue
             Retourne la vue choisie par l'utilisateur dans le terminal
         """
@@ -40,45 +40,66 @@ class RechercheUtilisateurVue(VueAbstraite):
             case "Entrer le pseudo de l'utilisateur recherché":
                 pseudo = inquirer.text(message="Entrer le pseudo : ").execute()
                 n = 0
-                while True:
-                    choix2 = RechercheService().recherche_utilisateur(pseudo, n)
+                a = 0
+                long = RechercheService().recherche_utilisateur(pseudo, n, a)["longueur"]
+                res_entier = (long // 8)
+                res_reste = long % 8
+
+                while n >= 0:
+                    choix2 = RechercheService().recherche_utilisateur(pseudo, n, a)["liste"]
 
                     if not choix2:
-                        print(f"Aucun manga trouvé pour le titre '{titre}'.")
+                        print(f"Aucun utilisateur trouvé pour le pseudo '{pseudo}'.")
                         break
 
                     choix2.extend(["Afficher la page suivante", "Retour au menu précédent"])
 
+                    if res_entier == 0 and res_reste == 0:
+                        choix2.remove("Afficher la page suivante")
+                        choix2.extend(["Afficher la page précédente"])
+
                     choix3 = inquirer.select(
-                                            message="Choisissez un utilisateur : ",
-                                            choices=choix2,
-                                            ).execute()
+                        message="Choisissez un utilisateur : ",
+                        choices=choix2,
+                    ).execute()
 
                     if choix3 == "Retour au menu précédent":
                         from vues.menu_utilisateur_vue import MenuUtilisateurVue
                         return MenuUtilisateurVue()
 
                     elif choix3 == "Afficher la page suivante":
-                        n += 8
+                        if res_entier != 0:
+                            n = n + 8
+                            res_entier -= 1
+                        if res_entier == 0 and res_reste != 0:
+                            n = n + 8
+                            a = res_reste - 8
+
+                    elif choix3 == "Afficher la page précédente":
+                        if res_entier == 0:
+                            n = n - 8
+                            res_entier += 1
+                        if res_entier != 0 and res_reste == 0:
+                            n = n - 8
+                            a = res_reste + 8
 
                     else:
+                        n = -1
                         choix4 = inquirer.select(
-                                            message="Faites votre choix : ",
-                                            choices=["Consulter les collections", "Consullter les avis", "Retour au menu précédent", "Retour vers l'écran d'accueil"],
-                                            ).execute()
+                            message="Faites votre choix : ",
+                            choices=["Consulter les collections", "Consulter les avis", "Retour au menu précédent", "Retour vers l'écran d'accueil"],
+                        ).execute()
                         match choix4:
                             case "Consulter les collections":
                                 pass
-                            case "Consullter les avis":
+                            case "Consulter les avis":
                                 pass
                             case "Retour au menu précédent":
                                 from vues.recherche_vue import RechercheVue
-
                                 return RechercheVue()
                             case "Retour vers l'écran d'accueil":
                                 from vues.menu_utilisateur_vue import MenuUtilisateurVue
-
                                 return MenuUtilisateurVue("Bon retour")
-                    from vues.menu_utilisateur_vue import MenuUtilisateurVue
 
-                    return MenuUtilisateurVue()
+        from vues.menu_utilisateur_vue import MenuUtilisateurVue
+        return MenuUtilisateurVue()
