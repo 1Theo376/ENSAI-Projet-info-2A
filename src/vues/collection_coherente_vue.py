6f99451d8c5c6a60c54f60b36f700fa8e56956cb
Emilien Morfouace <emilien.morfouace@eleve.ensai.fr>
2024-11-13 15:16:50 +0100

    merge

--- src/vues/collection_coherente_vue.py
+++ src/vues/collection_coherente_vue.py
@@ -2,7 +2,6 @@ from vues.vue_abstraite import VueAbstraite
 from InquirerPy import inquirer
 from vues.session import Session
 from service.recherche_service import RechercheService
-from dao.manga_dao import MangaDao
 
 
 class CollectionCoherenteVue(VueAbstraite):
@@ -18,8 +17,9 @@ class CollectionCoherenteVue(VueAbstraite):
         choix = RechercheService().recherche_collec_cohe_par_id(Session().utilisateur.id)
 
         if not choix:
-            print("Aucune collection trouvée")
+            print(f"Aucune collection trouvée")
             from vues.menu_utilisateur_vue import MenuUtilisateurVue
+
             return MenuUtilisateurVue().choisir_menu()
 
         choix.extend(["Retour au menu précédent", "Afficher la page suivante"])
@@ -30,9 +30,9 @@ class CollectionCoherenteVue(VueAbstraite):
         ).execute()
 
         if choix2 == "Retour au menu précédent":
-            from vues.menu_utilisateur_vue import MenuUtilisateurVue
+            from vues.profil_utilisateur_vue import EcranDuProfilVue
 
-            return MenuUtilisateurVue()
+            return EcranDuProfilVue()
 
         elif choix2 == "Afficher la page suivante":
             pass
