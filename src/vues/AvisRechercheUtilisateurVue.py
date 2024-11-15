f4d4b7bc444a8d347b4988e6f5d3ac5df14c3119
Théo Rougy <theo.rougy@eleve.ensai.fr>
2024-11-15 11:16:58 +0100

    z

--- src/vues/AvisRechercheUtilisateurVue.py
+++ src/vues/AvisRechercheUtilisateurVue.py
@@ -34,19 +34,30 @@ class AvisRechercheUtilisateurVue(VueAbstraite):
         print("\n" + "-" * 50 + "\nRecherche d'un utilisateur\n" + "-" * 50 + "\n")
         id_utilisateur = UtilisateurDao().recherche_id_par_pseudo(choix3)
         liste_avis, liste_titre = AvisService().recuperer_avis_utilisateur(id_utilisateur)
-        for i in range(len(liste_avis)):
-            print(
-                "\n" + "-" * 50 + f"\n{liste_avis[i]} Titre: {liste_titre[i]}\n" + "-" * 50 + "\n"
-                )
-        choixavis = inquirer.select(
-                                message="Faites votre choix : ",
-                                choices=["Retour au menu précédent", "Retour à l'accueil"],
-                                ).execute()
-        match choixavis:
-            case "Retour au menu précédent":
-                from vues.recherche_utilisateur_vue import RechercheUtilisateurVue
+        n = 0
+        m = 8
+        sous_liste_avis, sous_liste_titre, longueur_tot = liste_avis[n:n+m], liste_titre[n: n+m], len(liste_avis)
 
-                return RechercheUtilisateurVue()
-            case "Retour à l'accueil":
-                from vues.menu_utilisateur_vue import MenuUtilisateurVue
-                return MenuUtilisateurVue()
+        while n >= 0:
+            sous_liste_avis, sous_liste_titre, longueur_tot = liste_avis[n:n+m], liste_titre[n: n+m], len(liste_avis)
+            choix2 = ["Afficher la page suivante","Afficher la page précédente", "Retour au menu précédent"]
+            for i in range(n, n+m):
+                print(
+                    "\n" + "-" * 50 + f"\n{sous_liste_avis[i]} Titre: {sous_liste_titre[i]}\n" + "-" * 50 + "\n"
+                    )
+            if n + m >= longueur_tot:
+                choix2.remove("Afficher la page suivante")
+
+            if n == 0:
+                choix2.remove("Afficher la page précédente")
+
+            choix3 = inquirer.select(message="Choisissez une action :", choices=choix2).execute()
+
+            if choix3 == "Retour au menu précédent":
+                n = -1
+                from vues.recherche_vue import RechercheVue
+                return RechercheVue().choisir_menu()
+            elif choix3 == "Afficher la page suivante":
+                n += m
+            elif choix3 == "Afficher la page précédente":
+                n = max(0, n - m)
