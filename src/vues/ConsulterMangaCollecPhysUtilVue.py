79f030cab3d4e4eadc07d5eb80f888a6f42c0fb2
Théo Rougy <theo.rougy@eleve.ensai.fr>
2024-11-15 10:26:34 +0100

    e

--- src/vues/ConsulterMangaCollecPhysUtilVue.py
+++ src/vues/ConsulterMangaCollecPhysUtilVue.py
@@ -8,6 +8,7 @@ from dao.collection_coherente_dao import CollectionCoherenteDAO
 from dao.collection_physique_dao import CollectionPhysiqueDAO
 from dao.manga_dao import MangaDao
 
+
 class ConsulterMangaCollecPhysUtilVUe(VueAbstraite):
     def choisir_menu(self, choixu, choic):
         """Choix du menu suivant de l'utilisateur
@@ -20,8 +21,7 @@ class ConsulterMangaCollecPhysUtilVUe(VueAbstraite):
         print("\n" + "-" * 50 + "\nRecherche d'une collection\n" + "-" * 50 + "\n")
         id_utilisateur = UtilisateurDao().recherche_id_par_pseudo(choixu)
         liste_titre = []
-        for manga in (CollectionPhysiqueDAO().trouver_collec_phys_id_user(Session().utilisateur.id)
-                    ).Liste_manga:
+        for manga in (CollectionPhysiqueDAO().trouver_collec_phys_id_user(Session().utilisateur.id)).Liste_manga:
             liste_titre.append(MangaDao().trouver_manga_par_id(manga.idmanga).titre)
         choix4 = inquirer.select(
             message="Selectionnez un manga de votre collection : ",
