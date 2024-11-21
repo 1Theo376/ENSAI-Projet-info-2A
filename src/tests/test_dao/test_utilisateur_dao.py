8a0c354256b9b696d6d85b0b291eabf0655da764
Théo Rougy <theo.rougy@eleve.ensai.fr>
2024-11-21 10:57:50 +0100

    z

--- src/tests/test_dao/test_utilisateur_dao.py
+++ src/tests/test_dao/test_utilisateur_dao.py
@@ -1,8 +1,15 @@
 import pytest
+from unittest.mock import MagicMock
 from unittest.mock import patch
 from dao.utilisateur_dao import UtilisateurDao
 from business_object.utilisateur import Utilisateur
 
+
+liste_utilisateurs = [
+    Utilisateur(pseudo="huy", mdp="1234Azer"),
+    Utilisateur(pseudo="aze", mdp="0000Poiu"),
+    Utilisateur(pseudo="vert", mdp="abcd1Ruy"),
+]
 
 @pytest.fixture(scope="session", autouse=True)
 def setup_test_environment():
@@ -13,27 +20,31 @@ def setup_test_environment():
         yield
 
 
-def test_trouver_par_id_existant():
-    """Recherche par id d'un joueur existant"""
+def test_lister_tous_inclure_mdp_true():
+    """Lister les Joueurs en incluant les mots de passe"""
 
     # GIVEN
-    id_joueur = 998
+    UtilisateurDao().lister_tous = MagicMock(return_value=liste_utilisateurs)
 
     # WHEN
-    joueur = UtilisateurDao().trouver_par_id(id_joueur)
+    res = JoueurService().lister_tous(inclure_mdp=True)
 
     # THEN
-    assert joueur is not None
+    assert len(res) == 3
+    for joueur in res:
+        assert joueur.mdp is not None
 
 
-def test_trouver_par_id_non_existant():
-    """Recherche par id d'un joueur n'existant pas"""
+def test_lister_tous_inclure_mdp_false():
+    """Lister les Joueurs en excluant les mots de passe"""
 
     # GIVEN
-    id_joueur = 9999999999999
+    JoueurDao().lister_tous = MagicMock(return_value=liste_joueurs)
 
     # WHEN
-    joueur = JoueurDao().trouver_par_id(id_joueur)
+    res = JoueurService().lister_tous()
 
     # THEN
-    assert joueur is None
+    assert len(res) == 3
+    for joueur in res:
+        assert not joueur.mdp
