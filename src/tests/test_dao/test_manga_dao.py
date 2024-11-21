0c4f0f5d61fe00268c28e0f91032ad0dd500efa5
Théo Rougy <theo.rougy@eleve.ensai.fr>
2024-11-21 17:16:09 +0100

    a

--- src/tests/test_dao/test_manga_dao.py
+++ src/tests/test_dao/test_manga_dao.py
@@ -1,81 +1,84 @@
 import pytest
 from dao.manga_dao import MangaDao
-from business_object.manga import Manga
-from unittest.mock import MagicMock
 
 
-@pytest.fixture
-def manga_dao():
-    """Fixture pour creer une instance de MangaDao."""
-    return MangaDao()
+def test_trouver_manga_par_id_oui():
+    """Création de Utilisateur réussie"""
 
-
-def test_trouver_manga_par_id(manga_dao):
     # GIVEN
-    manga_dao.trouver_manga_par_id = MagicMock(
-        return_value=Manga(
-            id_manga=1,
-            titre="Naruto",
-            synopsis="Un jeune ninja rêve de devenir Hokage.",
-            auteur="Masashi Kishimoto",
-            themes="Aventure, Action",
-            genre="Shonen",
-        )
-    )
+    manga_dao = MangaDao()
+
     # WHEN
     manga = manga_dao.trouver_manga_par_id(1)
+
     # THEN
-    assert manga is not None
     assert manga.id_manga == 1
-    assert manga.titre == "Naruto"
-    assert manga.auteur == "Masashi Kishimoto"
 
 
-def test_trouver_manga_par_titre(manga_dao):
+def test_trouver_manga_par_id_non():
+    """Création de Utilisateur réussie"""
+
     # GIVEN
-    manga_dao.trouver_manga_par_titre = MagicMock(
-        return_value=Manga(
-            id_manga=2,
-            titre="One Piece",
-            synopsis="Luffy, un pirate en quête de trésors.",
-            auteur="Eiichiro Oda",
-            themes="Aventure, Comédie",
-            genre="Shonen",
-        )
-    )
+    manga_dao = MangaDao()
+
     # WHEN
-    manga = manga_dao.trouver_manga_par_titre("One Piece")
+    manga = manga_dao.trouver_manga_par_id(-1)
+
     # THEN
-    assert manga is not None
-    assert manga.titre == "One Piece"
-    assert manga.synopsis == "Luffy, un pirate en quête de trésors."
+    assert manga is None
 
 
-def test_rechercher_manga_par_titre(manga_dao):
-    # Mock de DBConnection pour retourner plusieurs mangas avec le même titre
-    manga_dao.rechercher_manga_par_titre = MagicMock(
-        return_value=[
-            Manga(
-                id_manga=1,
-                titre="Naruto",
-                synopsis="Un jeune ninja...",
-                auteur=None,
-                themes=None,
-                genre=None,
-            ),
-            Manga(
-                id_manga=2,
-                titre="Naruto Shippuden",
-                synopsis="Naruto revient plus fort.",
-                auteur=None,
-                themes=None,
-                genre=None,
-            ),
-        ]
-    )
-    result = manga_dao.rechercher_manga_par_titre("Naruto")
+def test_trouver_manga_par_titre_oui():
+    """Création de Utilisateur réussie"""
 
-    assert result is not None
-    assert len(result) == 2
-    assert result[0].titre == "Naruto"
-    assert result[1].titre == "Naruto Shippuden"
+    # GIVEN
+    manga_dao = MangaDao()
+
+    # WHEN
+    manga = manga_dao.trouver_manga_par_titre("Naruto")
+
+    # THEN
+    assert manga.titre == "Naruto"
+
+
+def test_trouver_manga_par_titre_non():
+    """Création de Utilisateur réussie"""
+
+    # GIVEN
+    manga_dao = MangaDao()
+
+    # WHEN
+    manga = manga_dao.trouver_manga_par_titre("zghefdthete")
+
+    # THEN
+    assert manga is None
+
+
+def test_rechercher_manga_par_titre_oui():
+    """Création de Utilisateur réussie"""
+
+    # GIVEN
+    manga_dao = MangaDao()
+
+    # WHEN
+    mangas = manga_dao.rechercher_manga_par_titre("Monster")
+
+    # THEN
+    assert len(mangas) > 10
+
+
+def test_rechercher_manga_par_titre_non():
+    """Création de Utilisateur réussie"""
+
+    # GIVEN
+    manga_dao = MangaDao()
+
+    # WHEN
+    mangas = manga_dao.rechercher_manga_par_titre("guyrazfgbyuy")
+
+    # THEN
+    assert mangas is None
+
+
+if __name__ == "__main__":
+    pytest.main([__file__])
