306255f4c1ac78d3f39e482de2ebb132f54b5117
Anais Babingui <ananashere0@gmail.com>
2024-11-22 20:53:30 +0100

    ook

--- src/tests/test_dao/test_collection_coherente_dao.py
+++ src/tests/test_dao/test_collection_coherente_dao.py
@@ -1,83 +1,196 @@
 import pytest
-from unittest.mock import patch
-from dao.collection_coherente_dao import CollectionCoherenteDAO
+from unittest.mock import patch, MagicMock
 from business_object.CollectionCoherente import CollectionCoherente
 from business_object.manga import Manga
+from business_object.utilisateur import Utilisateur
+from dao.collection_coherente_dao import CollectionCoherenteDAO
+from dao.utilisateur_dao import UtilisateurDao
+from dao.manga_dao import MangaDao
 
 
-@pytest.fixture
-def collection_dao():
-    return CollectionCoherenteDAO()
+utilisateur = Utilisateur("huy1", "1234Azer")
+
+liste_collection = [
+    CollectionCoherente("macollec1", "action", []),
+    CollectionCoherente("macollec2", "action", []),
+    CollectionCoherente("macollec3", "action", [])
+]
+
+collection = CollectionCoherente("manouvellecolec", "action", [])
 
 
-@pytest.fixture
-def collection_coherente():
-    return CollectionCoherente(
-        id_collectioncoherente=1,
-        titre_collection="Ma Collection",
-        desc_collection="Une collection d'anime populaire",
-        Liste_manga=[]
-    )
+@pytest.fixture(scope="function", autouse=True)
+def setup_test_environment():
+    """Initialisation des données de test pour UtilisateurDao"""
+    with patch.dict("os.environ", {"POSTGRES_SCHEMA": "projet_test_dao"}):
+        from utils.reset_database import ResetDatabase
+        ResetDatabase().lancer(test_dao=True)
+        MangaDao().inserer_mangas("testmangas.json")
+        yield
 
 
-@pytest.fixture
-def manga():
-    return Manga(
-        id_manga=1,
-        titre="Naruto",
-        synopsis="Un jeune ninja qui cherche à devenir Hokage.",
-        auteur="Masashi Kishimoto",
-        themes=["Aventure", "Action"],
-        genre="Shonen"
-    )
+@pytest.fixture(scope="function", autouse=True)
+def utilisateur_test():
+    """Crée un utilisateur pour les tests"""
+    return UtilisateurDao().creer(utilisateur)
 
 
-@patch("dao.db_connection.DBConnection")
-def test_creer_collection(mock_db, collection_dao, collection_coherente):
-    mock_connection = mock_db.return_value.connection
-    mock_cursor = mock_connection.cursor.return_value
-    mock_cursor.fetchone.return_value = {"id_collec_coherente": 1}
-
-    created = collection_dao.creer_collection(collection_coherente)
-
-    assert created
-    assert collection_coherente.id_collectioncoherente == 1
-    mock_cursor.execute.assert_called_once()
-    mock_cursor.fetchone.assert_called_once()
+@pytest.fixture(scope="function", autouse=True)
+def collection_test():
+    """Crée des collections pour les tests"""
+    id_utilisateur = 1
+    for collec in liste_collection:
+        CollectionCoherenteDAO().creer_collection(collec, id_utilisateur)
+    return liste_collection
 
 
-@patch("dao.db_connection.DBConnection")
-def test_supprimer_collection(mock_db, collection_dao, collection_coherente):
-    mock_connection = mock_db.return_value.connection
-    mock_cursor = mock_connection.cursor.return_value
-    mock_cursor.rowcount = 1
+def test_creer_collectionphys_oui():
+    """Création d'une collection physique dans la base de données"""
 
-    result = collection_dao.supprimer_collection(collection_coherente)
-
-    assert result is True
-    mock_cursor.execute.assert_called_once()
-
-
-@patch("dao.db_connection.DBConnection")
-def test_ajouter_manga(mock_db, collection_dao, collection_coherente, manga):
-    mock_connection = mock_db.return_value.connection
-    mock_cursor = mock_connection.cursor.return_value
-    mock_cursor.fetchone.return_value = {"id_collec_coherente": collection_coherente.id_collectioncoherente, "id_manga": manga.id_manga}
-    created = collection_dao.ajouter_manga(collection_coherente, manga)
-
-    assert created
-    assert manga.id_manga == 1
-    mock_cursor.execute.assert_called_once()
-    mock_cursor.fetchone.assert_called_once()
+    # GIVEN
+    id_utilisateur = 1
+    # WHEN
+    res = CollectionCoherenteDAO().creer_collection(collection, id_utilisateur)
+    # THEN
+    assert res
 
 
-@patch("dao.db_connection.DBConnection")
-def test_supprimer_manga(mock_db, collection_dao, collection_coherente, manga):
-    mock_connection = mock_db.return_value.connection
-    mock_cursor = mock_connection.cursor.return_value
-    mock_cursor.rowcount = 1
+def test_creer_collectionphys_non():
+    """Création d'une collection physique dans la base de données"""
 
-    result = collection_dao.supprimer_manga(collection_coherente, manga)
+    # GIVEN
+    id_utilisateur = 30
+    # WHEN
+    res = CollectionPhysiqueDAO().creer_collectionphys(collection, id_utilisateur)
+    # THEN
+    assert not res
 
-    assert result is True
-    mock_cursor.execute.assert_called_once()
+
+def test_supprimer_collectionphys_oui():
+    """Suppression d'une collection physique dans la base de données"""
+
+    # GIVEN
+    collection = Collection_physique("manouvellecolec", "action", [])
+    id_utilisateur = 1
+    CollectionPhysiqueDAO().creer_collectionphys(collection, id_utilisateur)
+    # WHEN
+    res = CollectionPhysiqueDAO().supprimer_collectionphys(1)
+    # THEN
+    assert res
+
+
+def test_supprimer_collectionphys_non():
+    """Suppression d'une collection physique dans la base de données"""
+
+    # GIVEN
+    collection = Collection_physique("manouvellecolec", "action", [])
+    id_utilisateur = 1
+    CollectionPhysiqueDAO().creer_collectionphys(collection, id_utilisateur)
+    # WHEN
+    res = CollectionPhysiqueDAO().supprimer_collectionphys(9999)
+    # THEN
+    assert not res
+
+
+def test_trouver_collec_phys_id_user_oui():
+    """Trouve une collection physique dans la base de données
+       selon l'identifiant de l'utilisateur"""
+    # GIVEN
+    collection = Collection_physique("manouvellecolec", "action", [])
+    id_utilisateur = 1
+    CollectionPhysiqueDAO().creer_collectionphys(collection, id_utilisateur)
+    # WHEN
+    res = CollectionPhysiqueDAO().trouver_collec_phys_id_user(id_utilisateur)
+    # THEN
+    assert res
+
+
+def test_trouver_collec_phys_id_user_non():
+    """Trouve une collection physique dans la base de données
+       selon l'identifiant de l'utilisateur"""
+    # GIVEN
+    collection = Collection_physique("manouvellecolec", "action", [])
+    id_utilisateur = 3
+    CollectionPhysiqueDAO().creer_collectionphys(collection, id_utilisateur)
+    # WHEN
+    res = CollectionPhysiqueDAO().trouver_collec_phys_id_user(id_utilisateur)
+    # THEN
+    assert not res
+
+
+def test_trouver_collec_phys_nom_oui():
+    """Trouve une collection physique dans la base de données
+       selon son titre"""
+    # GIVEN
+    collection = Collection_physique("manouvellecolec", "action", [])
+    id_utilisateur = 1
+    CollectionPhysiqueDAO().creer_collectionphys(collection, id_utilisateur)
+    # WHEN
+    res = CollectionPhysiqueDAO().trouver_collec_phys_nom("manouvellecolec")
+    # THEN
+    assert res
+
+
+def test_trouver_collec_phys_nom_non():
+    """Trouve une collection physique dans la base de données
+       selon son titre"""
+    # GIVEN
+    collection = Collection_physique("manouvellecolec", "action", [])
+    id_utilisateur = 1
+    CollectionPhysiqueDAO().creer_collectionphys(collection, id_utilisateur)
+    # WHEN
+    res = CollectionPhysiqueDAO().trouver_collec_phys_nom("manouvelle")
+    # THEN
+    assert not res
+
+
+def test_modifier_titre_oui():
+    """Modifie le titre d'une collection physique la base de données"""
+    # GIVEN
+    collection = Collection_physique("manouvellecolec", "action", [])
+    id_utilisateur = 1
+    CollectionPhysiqueDAO().creer_collectionphys(collection, id_utilisateur)
+    # WHEN
+    res = CollectionPhysiqueDAO().modifier_titre(1, "nouveautitre")
+    # THEN
+    assert res
+
+
+def test_modifier_titre_non():
+    """Modifie le titre d'une collection physique la base de données"""
+    # GIVEN
+    collection = Collection_physique("manouvellecolec", "action", [])
+    id_utilisateur = 1
+    CollectionPhysiqueDAO().creer_collectionphys(collection, id_utilisateur)
+    # WHEN
+    res = CollectionPhysiqueDAO().modifier_titre(34, "nouveautitre")
+    # THEN
+    assert not res
+
+
+def test_modifier_desc_oui():
+    """Modifie la description d'une collection physique la base de données"""
+    # GIVEN
+    collection = Collection_physique("manouvellecolec", "action", [])
+    id_utilisateur = 1
+    CollectionPhysiqueDAO().creer_collectionphys(collection, id_utilisateur)
+    # WHEN
+    res = CollectionPhysiqueDAO().modifier_desc(1, "nouvelle_desc")
+    # THEN
+    assert res
+
+
+def test_modifier_desc_non():
+    """Modifie la description d'une collection physique la base de données"""
+    # GIVEN
+    collection = Collection_physique("manouvellecolec", "action", [])
+    id_utilisateur = 1
+    CollectionPhysiqueDAO().creer_collectionphys(collection, id_utilisateur)
+    # WHEN
+    res = CollectionPhysiqueDAO().modifier_desc(34, "nouvelle_desc")
+    # THEN
+    assert not res
+
+
+if __name__ == "__main__":
+    pytest.main([__file__])
--- src/tests/test_dao/test_collection_physique_dao.py
+++ src/tests/test_dao/test_collection_physique_dao.py
@@ -1,11 +1,11 @@
 import pytest
 from unittest.mock import patch, MagicMock
 from business_object.collection_phys import Collection_physique
-from business_object.manga_possede import MangaPossede
 from business_object.manga import Manga
 from business_object.utilisateur import Utilisateur
 from dao.collection_physique_dao import CollectionPhysiqueDAO
 from dao.utilisateur_dao import UtilisateurDao
+from dao.manga_dao import MangaDao
 
 
 utilisateur = Utilisateur("huy1", "1234Azer")
@@ -19,22 +19,23 @@ liste_collection = [
 collection = Collection_physique("manouvellecolec", "action", [])
 
 
-@pytest.fixture(scope="session", autouse=True)
+@pytest.fixture(scope="function", autouse=True)
 def setup_test_environment():
     """Initialisation des données de test pour UtilisateurDao"""
     with patch.dict("os.environ", {"POSTGRES_SCHEMA": "projet_test_dao"}):
         from utils.reset_database import ResetDatabase
         ResetDatabase().lancer(test_dao=True)
+        MangaDao().inserer_mangas("testmangas.json")
         yield
 
 
-@pytest.fixture(scope="session", autouse=True)
+@pytest.fixture(scope="function", autouse=True)
 def utilisateur_test():
     """Crée un utilisateur pour les tests"""
     return UtilisateurDao().creer(utilisateur)
 
 
-@pytest.fixture(scope="session", autouse=True)
+@pytest.fixture(scope="function", autouse=True)
 def collection_test():
     """Crée des collections pour les tests"""
     id_utilisateur = 1
@@ -69,9 +70,9 @@ def test_supprimer_collectionphys_oui():
     """Suppression d'une collection physique dans la base de données"""
 
     # GIVEN
-    collection2 = Collection_physique("manouvellecolec2", "action", [])
+    collection = Collection_physique("manouvellecolec", "action", [])
     id_utilisateur = 1
-    CollectionPhysiqueDAO().creer_collectionphys(collection2, id_utilisateur)
+    CollectionPhysiqueDAO().creer_collectionphys(collection, id_utilisateur)
     # WHEN
     res = CollectionPhysiqueDAO().supprimer_collectionphys(1)
     # THEN
@@ -82,24 +83,111 @@ def test_supprimer_collectionphys_non():
     """Suppression d'une collection physique dans la base de données"""
 
     # GIVEN
-    collection3 = Collection_physique("manouvellecolec3", "action", [])
+    collection = Collection_physique("manouvellecolec", "action", [])
     id_utilisateur = 1
-    CollectionPhysiqueDAO().creer_collectionphys(collection3, id_utilisateur)
+    CollectionPhysiqueDAO().creer_collectionphys(collection, id_utilisateur)
     # WHEN
     res = CollectionPhysiqueDAO().supprimer_collectionphys(9999)
     # THEN
     assert not res
 
 
-def test_supprimer_mangaposs_oui():
-    """Suppression d'un manga possédé dans la base de données"""
-
+def test_trouver_collec_phys_id_user_oui():
+    """Trouve une collection physique dans la base de données
+       selon l'identifiant de l'utilisateur"""
     # GIVEN
-    collection3 = Collection_physique("manouvellecolec3", "action", [])
+    collection = Collection_physique("manouvellecolec", "action", [])
     id_utilisateur = 1
-    CollectionPhysiqueDAO().creer_collectionphys(collection3, id_utilisateur)
+    CollectionPhysiqueDAO().creer_collectionphys(collection, id_utilisateur)
     # WHEN
-    res = CollectionPhysiqueDAO().supprimer_mangaposs(9999)
+    res = CollectionPhysiqueDAO().trouver_collec_phys_id_user(id_utilisateur)
+    # THEN
+    assert res
+
+
+def test_trouver_collec_phys_id_user_non():
+    """Trouve une collection physique dans la base de données
+       selon l'identifiant de l'utilisateur"""
+    # GIVEN
+    collection = Collection_physique("manouvellecolec", "action", [])
+    id_utilisateur = 3
+    CollectionPhysiqueDAO().creer_collectionphys(collection, id_utilisateur)
+    # WHEN
+    res = CollectionPhysiqueDAO().trouver_collec_phys_id_user(id_utilisateur)
+    # THEN
+    assert not res
+
+
+def test_trouver_collec_phys_nom_oui():
+    """Trouve une collection physique dans la base de données
+       selon son titre"""
+    # GIVEN
+    collection = Collection_physique("manouvellecolec", "action", [])
+    id_utilisateur = 1
+    CollectionPhysiqueDAO().creer_collectionphys(collection, id_utilisateur)
+    # WHEN
+    res = CollectionPhysiqueDAO().trouver_collec_phys_nom("manouvellecolec")
+    # THEN
+    assert res
+
+
+def test_trouver_collec_phys_nom_non():
+    """Trouve une collection physique dans la base de données
+       selon son titre"""
+    # GIVEN
+    collection = Collection_physique("manouvellecolec", "action", [])
+    id_utilisateur = 1
+    CollectionPhysiqueDAO().creer_collectionphys(collection, id_utilisateur)
+    # WHEN
+    res = CollectionPhysiqueDAO().trouver_collec_phys_nom("manouvelle")
+    # THEN
+    assert not res
+
+
+def test_modifier_titre_oui():
+    """Modifie le titre d'une collection physique la base de données"""
+    # GIVEN
+    collection = Collection_physique("manouvellecolec", "action", [])
+    id_utilisateur = 1
+    CollectionPhysiqueDAO().creer_collectionphys(collection, id_utilisateur)
+    # WHEN
+    res = CollectionPhysiqueDAO().modifier_titre(1, "nouveautitre")
+    # THEN
+    assert res
+
+
+def test_modifier_titre_non():
+    """Modifie le titre d'une collection physique la base de données"""
+    # GIVEN
+    collection = Collection_physique("manouvellecolec", "action", [])
+    id_utilisateur = 1
+    CollectionPhysiqueDAO().creer_collectionphys(collection, id_utilisateur)
+    # WHEN
+    res = CollectionPhysiqueDAO().modifier_titre(34, "nouveautitre")
+    # THEN
+    assert not res
+
+
+def test_modifier_desc_oui():
+    """Modifie la description d'une collection physique la base de données"""
+    # GIVEN
+    collection = Collection_physique("manouvellecolec", "action", [])
+    id_utilisateur = 1
+    CollectionPhysiqueDAO().creer_collectionphys(collection, id_utilisateur)
+    # WHEN
+    res = CollectionPhysiqueDAO().modifier_desc(1, "nouvelle_desc")
+    # THEN
+    assert res
+
+
+def test_modifier_desc_non():
+    """Modifie la description d'une collection physique la base de données"""
+    # GIVEN
+    collection = Collection_physique("manouvellecolec", "action", [])
+    id_utilisateur = 1
+    CollectionPhysiqueDAO().creer_collectionphys(collection, id_utilisateur)
+    # WHEN
+    res = CollectionPhysiqueDAO().modifier_desc(34, "nouvelle_desc")
     # THEN
     assert not res
 
